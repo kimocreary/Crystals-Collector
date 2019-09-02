c71b16575512080d12bfd8cc6b4daa7b306382b8
Kimberlyn Creary <kimocreary@gmail.com>
2019-09-02 18:36:06 -0400

    jquery and html added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,49 @@
+var random_result;
+var lost = 0;
+var win = 0;
+var previous = 0;
 
+var resetAndStart = function (){
+    $(".crystals").empty();
+    random_result = Math.floor (Math.random () * 90) + 30;
 
+    $("#result").html('Random Result: ' + random_result);
 
+    for (var i = 0; i < 4; i++){
+        var random = Math.floor(Math.random () * 12) + 1;
+
+        var crystal = $("<div>");
+
+            crystal.attr ({
+                "class": 'crystal'
+                "data-random": random
+            });
+                crystal.html (random);
+            $(".crystals").append(crystal);
+    }
+}
+
+resetAndStart ();
+
+var reset = function () {
+    $(document).on('click', ".crystal", function (){
+        var num = parseInt ($(this).attr('data-random'));
+        previous += num;
+        console.log(previous);
+
+        if (previous > random_result){
+            lost--;
+            $("#lost").html (lost);
+            previous = 0
+            resetAndStart ();
+        }else if (previous===random_result){
+            win++;
+            $("#win").html(win);
+            previous = 0;
+            resetAndStart
+        }
+    })
+}
 
 
 
