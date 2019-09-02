3bca5056757d2513a81fdbf6cb013fa2bafaf692
Kimberlyn Creary <kimocreary@gmail.com>
2019-09-02 13:42:55 -0400

    psuedocode added

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+// Computer select randomly generated number between 19 - 120
+// Assign a randomly generated number to each of the four crystals between 1-12
+// Add an on click to each crystal
+// Print the sum of the clicked numbers
+// If the sum is < randomly generated number, allow user to keep guessing
+// If the sum is equal to the randomly generated number, add to wins 
+// If the sum is greater than the randomly generated number, add to losses
