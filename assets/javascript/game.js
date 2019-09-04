0f6fc7723d08e7d46dae09d737991e88ecc235cc
Kimberlyn Creary <kimocreary@gmail.com>
2019-09-03 21:48:29 -0400

    images added and html linked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,48 +2,61 @@ var random_result;
 var lost = 0;
 var win = 0;
 var previous = 0;
+var crystalCollected = 0;
 
 var resetAndStart = function (){
-    $(".crystals").empty();
+    // $(".crystals").empty();
     random_result = Math.floor (Math.random () * 90) + 30;
 
-    $("#result").html('Random Result: ' + random_result);
+    $("#crystalValue").html(random_result);
 
-    for (var i = 0; i < 4; i++){
-        var random = Math.floor(Math.random () * 12) + 1;
+    // for (var i = 0; i < 4; i++){
+        var random1 = Math.floor(Math.random () * 12) + 1;
+        var random2 = Math.floor(Math.random () * 12) + 1;
+        var random3 = Math.floor(Math.random () * 12) + 1;
+        var random4 = Math.floor(Math.random () * 12) + 1;
 
-        var crystal = $("<div>");
+        $("#image1").attr("data-value",random1);
+        $("#image2").attr("data-value",random2);
+        $("#image3").attr("data-value",random3);
+        $("#image4").attr("data-value",random4);
 
-            crystal.attr ({
-                "class": 'crystal'
-                "data-random": random
-            });
-                crystal.html (random);
-            $(".crystals").append(crystal);
-    }
+
+    //     var crystal = $("<div>");
+
+    //         crystal.attr ({
+    //             "class": 'crystal'
+    //             "data-random": random
+    //         });
+    //             crystal.html (random);
+    //         $(".crystals").append(crystal);
+    // }
 }
 
 resetAndStart ();
 
-var reset = function () {
-    $(document).on('click', ".crystal", function (){
-        var num = parseInt ($(this).attr('data-random'));
-        previous += num;
-        console.log(previous);
-
-        if (previous > random_result){
-            lost--;
-            $("#lost").html (lost);
-            previous = 0
-            resetAndStart ();
-        }else if (previous===random_result){
+// var reset = function () {
+    $("img").on('click', function (){
+        var num = parseInt($(this).attr('data-value'));
+        crystalCollected += num;
+        console.log(crystalCollected);
+        $("#crystalCollected").text(crystalCollected)
+        
+        if (crystalCollected == random_result){
             win++;
             $("#win").html(win);
-            previous = 0;
-            resetAndStart
+            console.log("wins",win)
+            crystalCollected = 0;
+            resetAndStart();
+        }
+        else if (crystalCollected > random_result){
+            lost++;
+            $("#lost").html(lost);
+            crystalCollected = 0
+            resetAndStart();
         }
     })
-}
+// }
 
 
 
